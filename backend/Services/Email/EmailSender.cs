df9577da11d2c744938be7ed9cac31e61dc91a8e
Mihai Niculai <nmbroomy@gmail.com>
2023-11-08 00:52:47 +0200

    Add EmailSettings class

--- backend/Services/Email/EmailSender.cs
+++ backend/Services/Email/EmailSender.cs
@@ -78,13 +78,5 @@ namespace Swallow.Services.Email
         }
     }
 
-    public class EmailSettings
-    {
-        public required string From { get; set; }
-        public required string SmtpServer { get; set; }
-        public required int Port { get; set; }
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-        public required bool EnableSSL { get; set; }
-    }
+    
 }
---
+++ backend/Services/Email/EmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Swallow.Services.Email
+{
+    public class EmailSettings
+    {
+        public required string From { get; set; }
+        public required string SmtpServer { get; set; }
+        public required int Port { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required bool EnableSSL { get; set; }
+    }
+}
