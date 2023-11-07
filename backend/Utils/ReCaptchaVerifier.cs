5ca2fc9a35fd244114c483f100f00e9304963011
Mihai Niculai <nmbroomy@gmail.com>
2023-11-08 00:45:11 +0200

    Add Google reCAPTCHA

---
+++ backend/Utils/ReCaptchaVerifier.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Swallow.Utils
+{
+    public class ReCaptchaVerifier
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _reCaptchaSecret;
+
+        public ReCaptchaVerifier(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _reCaptchaSecret = configuration["Google:ReCaptchaSecretKey"] ?? "";
+        }
+
+        public async Task<bool> VerifyAsync(string reCaptchaToken)
+        {
+            var reCaptchaVerificationUrl = "https://www.google.com/recaptcha/api/siteverify";
+            var postData = new FormUrlEncodedContent(new[]
+            {
+            new KeyValuePair<string, string>("secret", _reCaptchaSecret),
+            new KeyValuePair<string, string>("response", reCaptchaToken)
+        });
+
+            var response = await _httpClient.PostAsync(reCaptchaVerificationUrl, postData);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResult = await response.Content.ReadAsStringAsync();
+                var result = JObject.Parse(jsonResult);
+                return (bool)result["success"];
+            }
+
+            return false;
+        }
+    }
+}
