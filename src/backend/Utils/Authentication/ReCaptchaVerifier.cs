acbbe13f290ac28b9cca3facb683ddaf29cc9aac
Mihai Niculai <nmbroomy@gmail.com>
2024-02-26 07:50:04 +0200

    Refactor code indent

--- src/backend/Utils/Authentication/ReCaptchaVerifier.cs
+++ src/backend/Utils/Authentication/ReCaptchaVerifier.cs
@@ -9,18 +9,20 @@ namespace Swallow.Utils.Authentication
         public async Task<bool> VerifyAsync(string reCaptchaToken)
         {
             var reCaptchaVerificationUrl = "https://www.google.com/recaptcha/api/siteverify";
-            var postData = new FormUrlEncodedContent(new[]
-            {
-            new KeyValuePair<string, string>("secret", _reCaptchaSecret),
-            new KeyValuePair<string, string>("response", reCaptchaToken)
-            });
+
+            var postData = new FormUrlEncodedContent(
+            [
+                new KeyValuePair<string, string>("secret", _reCaptchaSecret),
+                new KeyValuePair<string, string>("response", reCaptchaToken)
+            ]);
 
             var response = await httpClient.PostAsync(reCaptchaVerificationUrl, postData);
+
             if (response.IsSuccessStatusCode)
             {
                 var jsonResult = await response.Content.ReadAsStringAsync();
                 var result = JObject.Parse(jsonResult);
-                return (bool)result["success"];
+                return (bool)result["success"]!;
             }
 
             return false;
